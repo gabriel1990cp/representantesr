d5767addcb2558c73d94e8f7577623bd2d862907
Gabriel Costa <gcosta@uplexis.com.br>
2020-07-28 17:57:04 -0300

    Telas finalizado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <h5 class="card-header">Entrada de pedido</h5>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -13,125 +13,145 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                        <form>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">Representante</label>
-                                    <input type="email" class="form-control" id="inputEmail4">
-                                </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Representante</label>
+                                <input type="email" class="form-control" id="inputEmail4">
                             </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">CNPJ Cliente</label>
-                                    <input type="email" class="form-control" id="inputEmail4">
-                                </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">CNPJ Cliente</label>
+                                <input type="email" class="form-control" id="inputEmail4">
                             </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">Data pedido</label>
-                                    <input type="date" class="form-control" id="inputEmail4">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">Local retirada</label>
-                                    <select id="inputEstado" class="form-control">
-                                        <option selected disabled>Selecione</option>
-                                        <option>Matriz</option>
-                                        <option>Filial</option>
-                                    </select>
-                                </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Data pedido</label>
+                                <input type="date" class="form-control" id="inputEmail4">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Local retirada</label>
+                                <select id="inputEstado" class="form-control">
+                                    <option selected disabled>Selecione</option>
+                                    <option>Matriz</option>
+                                    <option>Filial</option>
+                                </select>
+                            </div>
 
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Valor total</label>
+                                <input type="email" class="form-control" id="inputEmail4">
                             </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label for="inputEmail4">Valor total</label>
-                                    <input type="email" class="form-control" id="inputEmail4">
-                                </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label for="inputEmail4">Data de entrega</label>
+                                <input type="date" class="form-control" id="inputEmail4">
                             </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-4">
-                                    <label for="inputEmail4">Data de entrega</label>
-                                    <input type="date" class="form-control" id="inputEmail4">
-                                </div>
-                                <div class="form-group col-md-4">
-                                    <label for="inputEmail4">Cond. PGTO</label>
-                                    <input type="text" class="form-control" id="inputEmail4">
-                                </div>
-                                <div class="form-group col-md-4">
-                                    <label for="inputEmail4">Desconto aplicado</label>
-                                    <input type="text" class="form-control" id="inputEmail4">
-                                </div>
+                            <div class="form-group col-md-4">
+                                <label for="inputEmail4">Cond. PGTO</label>
+                                <input type="text" class="form-control" id="inputEmail4">
                             </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-3">
-                                    <label for="inputEmail4">Frete por conta de</label>
-                                    <select id="inputEstado" class="form-control">
-                                        <option selected disabled>Selecione</option>
-                                        <option>Matriz</option>
-                                        <option>Filial</option>
-                                    </select>
-                                </div>
-                                <div class="form-group col-md-3">
-                                    <label for="inputEmail4">Data de entrega</label>
-                                    <input type="date" class="form-control" id="inputEmail4">
-                                </div>
-                                <div class="form-group col-md-3">
-                                    <label for="inputEmail4">Número cliente</label>
-                                    <input type="number" class="form-control" id="inputEmail4">
-                                </div>
-                                <div class="form-group col-md-3">
-                                    <label for="inputEmail4">Valor total</label>
-                                    <input type="number" class="form-control" id="inputEmail4">
-                                </div>
+                            <div class="form-group col-md-4">
+                                <label for="inputEmail4">Desconto aplicado</label>
+                                <input type="text" class="form-control" id="inputEmail4">
                             </div>
-                            <div class="form-row">
-                                <div class="form-group col-md-12">
-                                    <label for="inputEmail4">Adicionar Item</label>
-                                    <select id="inputEstado" class="form-control">
-                                        <option selected disabled>Selecione</option>
-                                        <option>Matriz</option>
-                                        <option>Filial</option>
-                                    </select>
-                                </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-3">
+                                <label for="inputEmail4">Frete por conta de</label>
+                                <select id="inputEstado" class="form-control">
+                                    <option selected disabled>Selecione</option>
+                                    <option>Matriz</option>
+                                    <option>Filial</option>
+                                </select>
                             </div>
-                            <button type="submit" class="btn btn-primary">Adicionar</button>
-                            <div class="form-row">
-                                <div class="form-group col-md-12">
-                                    <label for="inputEmail4"></label>
-                                    <table class="table table-bordered">
-                                        <thead>
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">First</th>
-                                            <th scope="col">Last</th>
-                                            <th scope="col">Handle</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td colspan="2">Larry the Bird</td>
-                                            <td>@twitter</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
+                            <div class="form-group col-md-3">
+                                <label for="inputEmail4">Data de entrega</label>
+                                <input type="date" class="form-control" id="inputEmail4">
                             </div>
-                        </form>
+                            <div class="form-group col-md-3">
+                                <label for="inputEmail4">Número cliente</label>
+                                <input type="number" class="form-control" id="inputEmail4">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label for="inputEmail4">Valor total</label>
+                                <input type="number" class="form-control" id="inputEmail4">
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            Adicionar produto
+                        </button>
+                        <div class="form-row">
+                            <div class="form-group col-md-12">
+                                <label for="inputEmail4"></label>
+                                <table class="table table-bordered">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">Cod</th>
+                                        <th scope="col">Descrição</th>
+                                        <th scope="col">UM</th>
+                                        <th scope="col">Quantidade</th>
+                                        <th scope="col">Valor un.</th>
+                                        <th scope="col">Valor un. sugerido</th>
+                                        <th scope="col">Valor total</th>
+                                        <th scope="col">Ação</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">ABC123</th>
+                                        <td>Bola azul</td>
+                                        <td>UM</td>
+                                        <td>10</td>
+                                        <td>5,30</td>
+                                        <td><input type="text" class="form-control" id="inputEmail4" value="8,30"></td>
+                                        <td>80,30</td>
+                                        <td>
+                                            <button class="btn btn-danger">Deletar</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">ABC123</th>
+                                        <td>Bola roxa</td>
+                                        <td>UM</td>
+                                        <td>10</td>
+                                        <td>5,30</td>
+                                        <td><input type="text" class="form-control" id="inputEmail4" value="8,30"></td>
+                                        <td>80,30</td>
+                                        <td>
+                                            <button class="btn btn-danger">Deletar</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">ABC123</th>
+                                        <td>Bola amarela</td>
+                                        <td>UM</td>
+                                        <td>10</td>
+                                        <td>5,30</td>
+                                        <td><input type="text" class="form-control" id="inputEmail4" value="8,30"></td>
+                                        <td>80,30</td>
+                                        <td>
+                                            <button class="btn btn-danger">Deletar</button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            Salvar entrada de pedido
+                        </button>
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                            Cancelar
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    @include('modal')
 @endsection
---
+++ resources/views/modal.blade.php
@@ -0,0 +1,84 @@
+<!-- Modal -->
+<div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog"
+     aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Procurar produto</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-row">
+                    <div class="form-group col-md-12">
+                        <label for="inputEmail4">Descrição</label>
+                        <input type="email" class="form-control" id="inputEmail4">
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="inputEmail4">Código</label>
+                        <input type="email" class="form-control" id="inputEmail4">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputEmail4">Grupo</label>
+                        <input type="email" class="form-control" id="inputEmail4">
+                    </div>
+                </div>
+                <button type="button" class="btn btn-primary">Pesquisar</button>
+                <div class="form-row">
+                    <div class="form-group col-md-12">
+                        <label for="inputEmail4"></label>
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th scope="col">Cod</th>
+                                <th scope="col">Descrição</th>
+                                <th scope="col">Grupo</th>
+                                <th scope="col">Quantidade</th>
+                                <th scope="col">Ação</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">ABC123</th>
+                                <td>Bola azul</td>
+                                <td>UM</td>
+                                <td><input type="email" class="form-control" id="inputEmail4"></td>
+                                <td>
+                                    <button type="submit" class="btn btn-primary">Adicionar</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">ABC123</th>
+                                <td>Bola azul</td>
+                                <td>UM</td>
+                                <td><input type="email" class="form-control" id="inputEmail4"></td>
+                                <td>
+                                    <button type="submit" class="btn btn-primary">Adicionar</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">ABC123</th>
+                                <td>Bola azul</td>
+                                <td>UM</td>
+                                <td><input type="email" class="form-control" id="inputEmail4"></td>
+                                <td>
+                                    <button type="submit" class="btn btn-primary">Adicionar</button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
