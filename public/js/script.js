f47aaa6863fcc493c757a5a4d926091897699424
Gabriel Costa <gcosta@uplexis.com.br>
2020-08-11 13:50:35 -0300

    Adicionado public

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $('#btn-pesquisar-produto').click(function () {
+
+        //$("#products-table tr").detach();
+        $("#products-table tbody tr").remove();
+
+        $description = $("#id-descricao").val();
+        $code = $("#id-codigo").val();
+
+        $.ajax
+        ({
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify({description: $description, code: $code}),
+            url: 'api/searchProduct',
+            success: function (retorno) {
+                $.each(retorno['data'], function (index, value) {
+                    newRowTable(index, value);
+                });
+            },
+            error: function (error) {
+                alert('erro');
+            }
+        });
+    });
+
+
+    function newRowTable(index, value)
+    {
+        var newRow = $("<tr>");
+        var cols = "";
+        cols += '<td>' + value['itm'] + '</td>';
+        cols += '<td>' + value['dsc1'] + '</td>';
+        cols += '<td>' + value['srp1'] + '</td>';
+        cols += '<td>' + 0 + '</td>';
+        cols += '<td><button type="submit" class="btn btn-primary">Adicionar</button></td>';
+        newRow.append(cols);
+        $("#products-table").append(newRow);
+    }
+
+});
+
